f2ed260f7202df57431f627cbabf4e67efa30a36
avinashsingh1998 <44994562+avinashsingh1998@users.noreply.github.com>
2022-10-23 13:35:08 +0530

    third committed

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const colors = require("colors");
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 const app = express();
+const connectDB = require("./config/db");
+const { errorHandler } = require("./middleware/errorMiddleware");
+
+//connect to database
+connectDB();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "welcome to the support desk api" });
@@ -9,5 +18,8 @@ app.get("/", (req, res) => {
 
 //Routes
 app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/tickets", require("./routes/ticketRoutes"));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
